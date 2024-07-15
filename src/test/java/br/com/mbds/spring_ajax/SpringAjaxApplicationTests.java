48b410d7fadce01deadb38a60ac0ae8fc94783d2
Eduardo Belem <edumanoel@gmail.com>
2024-07-15 16:49:05 -0300

    first commit

---
+++ src/test/java/br/com/mbds/spring_ajax/SpringAjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mbds.spring_ajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
